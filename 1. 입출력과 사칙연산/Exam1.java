07f17007d05345ca559e5d644e0a3acea68230c8
Yechan Bark <102140367+pyc970905@users.noreply.github.com>
2022-12-11 14:39:52 +0900

    Create Exam1.java

---
+++ 1. 입출력과 사칙연산/Exam1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+//1. Hello World (Hello World!를 화면에 출력하는 문제)
+public class Practice {
+    public static void main(String []args){
+        System.out.println("Hello World!");
+    }
+}
