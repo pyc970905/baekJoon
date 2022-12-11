901451a4bf9e9fae01a832cfc0088252fc81f488
Yechan Bark <102140367+pyc970905@users.noreply.github.com>
2022-12-11 14:45:49 +0900

    Create Exam4.java

---
+++ 1. 입출력과 사칙연산/Exam4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Exam4 {
+
+    //4. A*B (두 수를 입력받고 곱셈을 한 결과를 출력하는 문제)
+    public static void main(String []args){
+
+        //(0 < A, B < 10)
+        Scanner input = new Scanner(System.in);
+
+        Integer a,b;
+
+        a = input.nextInt();
+        b = input.nextInt();
+
+        if(a > 0 && b < 10 ) {
+            System.out.println(a * b);
+        }
+        else if (a <= 0){
+            System.out.println("숫자 a의 범위가 맞지 않습니다.");
+        }
+
+        else if ( b >= 10 ) {
+            System.out.println("숫자 b의 범위가 맞지 않습니다.");
+        }
+
+    }
