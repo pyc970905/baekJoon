82d18a0f9a0951e4ed512e6c2175647a095026ec
Yechan Bark <102140367+pyc970905@users.noreply.github.com>
2022-12-11 14:41:42 +0900

    Create Exam3.java

---
+++ 1. 입출력과 사칙연산/Exam3.java
@@ -0,0 +1,25 @@
+public class Exam3 {
+
+    //3. A-B (두 수를 입력받고 뺄셈을 한 결과를 출력하는 문제)
+    public static void main(String []args){
+
+        //(0 < A, B < 10)
+        Scanner input = new Scanner(System.in);
+
+        Integer a,b;
+
+        a = input.nextInt();
+        b = input.nextInt();
+
+        if(a > 0 && b < 10 ) {
+            System.out.println(a - b);
+        }
+        else if (a <= 0){
+            System.out.println("숫자 a의 범위가 맞지 않습니다.");
+        }
+
+        else if ( b >= 10 ) {
+            System.out.println("숫자 b의 범위가 맞지 않습니다.");
+        }
+
+    }
